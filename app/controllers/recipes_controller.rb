6331593d0fdce13dd169b235337363e1ace52e48
asengelhart <asengelhart@yahoo.com>
2020-03-11 17:49:12 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,12 +5,33 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
+    2.times { @recipes.ingredients.build }
+    @ingredients = @recipes.ingredients
   end
 
   def new
     @recipe = Recipe.new
+    @ingredients = @recipe.tap{ |m| 2.times{m.ingredients.build} }.ingredients
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
